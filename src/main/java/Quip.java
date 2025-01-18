3a6ab2c6a03e52fbff8ce1bd00971b476970c1fe
Mooncheez <62787982+johngao122@users.noreply.github.com>
2025-01-18 13:56:19 +0800

    level-0

---
+++ src/main/java/Quip.java
@@ -0,0 +1,28 @@
+public class Quip {
+    private static final String NAME = "Quip";
+    private static final String LINE = "____________________________________________________________";
+
+    private static void greet() {
+        String logo = "________        .__        \n" +
+                "\\_____  \\  __ __|__|_____  \n" +
+                " /  / \\  \\|  |  \\  \\____ \\ \n" +
+                "/   \\_/.  \\  |  /  |  |_> >\n" +
+                "\\_____\\ \\_/____/|__|   __/ \n" +
+                "       \\__>        |__|    \n";
+        System.out.println(logo);
+        System.out.println("Hi there human! I'm " + NAME + "!");
+        System.out.println("What shenanigans can I help you with today?");
+        System.out.println(LINE);
+    }
+
+    private static void exit() {
+        System.out.println("Aww, you’re leaving already? \uD83D\uDE22 Bye for now!");
+        System.out.println(LINE);
+    }
+
+
+    public static void main(String[] args) {
+        greet();
+        exit();
+    }
+}
