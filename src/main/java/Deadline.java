e4241274c083f639f588805ed4a8a382a9e95737
Mooncheez <62787982+johngao122@users.noreply.github.com>
2025-01-26 22:51:04 +0800

    Integrate datetime into event

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,19 @@
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 class Deadline extends Task {
     private final LocalDateTime deadline;
-    private final DateTimeFormatter DISPLAY_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm a");
-    private final DateTimeFormatter FILE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private final DateTimeFormatter DISPLAY_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd h:mm a");
+    private final DateTimeFormatter FILE_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
     public Deadline(String task, String deadline) throws QuipException {
         super(task, TaskType.DEADLINE);
         try {
             this.deadline = LocalDateTime.parse(deadline, FILE_FORMATTER);
-        } catch (DateTimeException e) {
+        } catch (DateTimeParseException e) {
             throw new QuipException("Please use this format: yyyy-MM-dd HH:mm");
         }
     }
